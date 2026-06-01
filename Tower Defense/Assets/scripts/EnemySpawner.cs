33643012bafb55f909d71843cd14ef025e747d61
Katya Markina <Katya.Markina@scsstudent.org>
2026-06-01 12:26:37 -0700

    feat: Work in progress vid 9

---
+++ Tower Defense/Assets/scripts/EnemySpawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [field: SerializeField]
+    public EnemyMovement Enemy { get; private set; }
+
+    [field: SerializeField]
+    public Waypoint StartingWaypoint { get; private set; }
+
+    [field: SerializeField]
+    public float Delay { get; private set; } = 5f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+        EnemyMovement newEnemy = Object.Instantiate(Enemy);
+        newEnemy.Target = StartingWaypoint;
+    }
+}
